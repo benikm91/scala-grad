248c4e3f1bf1dda8568a79a1632c61c4e3f9b9e7
Benjamin Meyer <benikm91@gmail.com>
2023-08-26 22:46:08 +0200

    code clean up

--- scala-grad-auto-breeze/src/scalagrad/auto/breeze/BreezeFloatMatrixAlgebraDSL.scala
+++ scala-grad-auto-breeze/src/scalagrad/auto/breeze/BreezeFloatMatrixAlgebraDSL.scala
@@ -14,13 +14,9 @@ val mtEF = summon[Typeable[DenseMatrix[Float]]]
 
 object BreezeFloatMatrixAlgebraDSL extends MatrixAlgebraDSL:
     
-
     override given st: Typeable[Float] = stEF
-
     override given cvt: Typeable[DenseVector[Float]] = cvtEF
-
     override given rvt: Typeable[Transpose[DenseVector[Float]]] = rvtEF
-
     override given mt: Typeable[DenseMatrix[Float]] = mtEF
 
     override type Scalar = Float
